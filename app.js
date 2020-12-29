70e9d9929ab08cc60a03c02076c861327c0fd3aa
SakshiKabra55 <72736824+SakshiKabra55@users.noreply.github.com>
2020-12-29 18:33:45 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const https=require("https");
+const app=express();
+app.get("/",function(req,res){
+    const url="https://api.openweathermap.org/data/2.5/weather?q=London&appid=f15eaeba3emsh99bb44e4eac01e4p12b5d2jsnebfd4d991a44&units=metric";
+    https.get(url, function(response){
+        console.log(response.statusCode); 
+        response.on("data",function(data){
+            const weatherData=JSON.parse(data);
+            console.log(weatherData);
+        })
+    })
+    res.send("Server is up and running");
+})
+app.listen(3000,function(){
+    console.log("Server is running on port 3000");
+})
